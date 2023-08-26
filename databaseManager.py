ad00e4e99673f4a72e794d10f8b33997a6181c94
Nils Briggen <nilsbriggen@gmail.com>
2023-08-26 12:27:31 +0200

    updated mediApi for larger scale use
    I've implemented the database manager for a larger scale implementation aswell as updated the mediApi to use the database

---
+++ databaseManager.py
@@ -0,0 +1,21 @@
+class Manager:
+    def __init__(self):
+        self.chats = {}
+
+    def init_chat(self, patient_id):
+        self.chats[patient_id] = []
+
+    def add_message(self, patient_id, message):
+        self.chats[patient_id].append(message)
+
+    def get_chat(self, patient_id):
+        return self.chats[patient_id]
+
+    def remove_chat(self, patient_id):
+        del self.chats[patient_id]
+
+    def chat_exists(self, patient_id):
+        return patient_id in self.chats
+    
+    def get_latest_message(self, patient_id):
+        return self.chats[patient_id][-1]
